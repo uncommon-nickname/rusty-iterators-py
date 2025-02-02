38f2473a87b743f47f496cb63722a17fa0793b4e
leghart <leghart@wp.pl>
2025-02-02 21:58:12 +0100

    review fixes

--- rusty_iterators/core/wrappers.pyi
+++ rusty_iterators/core/wrappers.pyi
@@ -1,9 +1,9 @@
 from __future__ import annotations
 
-from collections.abc import Callable, Sequence
-from typing import Any, Generic, Iterator, TypeAlias, final
+from collections.abc import Callable, Iterator, Sequence
+from typing import Any, Generic, TypeAlias, final
 
-from rusty_iterators._versioned_types import TypeVar
+from rusty_iterators._versioned_types import TypeVar, override
 from rusty_iterators.core.interface import IterInterface
 
 T = TypeVar("T", default=Any, covariant=True)
@@ -15,9 +15,11 @@ MapCallable: TypeAlias = Callable[[T], R]
 @final
 class SeqWrapper(IterInterface[T], Generic[T]):
     def __init__(self, s: Sequence[T]) -> None: ...
-    def copy(self) -> bool: ...
+    @override
+    def copy(self) -> SeqWrapper[T]: ...
 
 @final
 class IterWrapper(IterInterface[T], Generic[T]):
     def __init__(self, it: Iterator[T]) -> None: ...
-    def copy(self) -> bool: ...
+    @override
+    def copy(self) -> IterWrapper[T]: ...
