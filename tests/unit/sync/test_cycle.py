670e01409f177e711649dbf3333f3d7b44c532cb
WiktorNowak <wnowak2f@gmail.com>
2025-02-04 07:04:18 +0100

    tests: add missing copy tests to cycle iterator

--- tests/unit/sync/test_cycle.py
+++ tests/unit/sync/test_cycle.py
@@ -6,7 +6,6 @@ def test_default_iterator_returns_cache_based_cycle() -> None:
     _iter = (x for x in [1, 2, 3])
     it = LIter.from_it(_iter).cycle()
 
-    assert not it.can_be_copied()
     assert isinstance(it, CacheCycle)
     assert [it.next() for _ in range(7)] == [1, 2, 3, 1, 2, 3, 1]
 
@@ -23,3 +22,29 @@ def test_when_specified_cycle_returns_copy() -> None:
 
     assert isinstance(it, CopyCycle)
     assert [it.next() for _ in range(7)] == ["a", "b", "c", "a", "b", "c", "a"]
+
+
+def test_copy_cycle_can_be_copied() -> None:
+    it = LIter.from_items(1, 2).cycle(use_cache=False)
+    it.next()
+
+    assert it.can_be_copied()
+
+    cp = it.copy()
+
+    assert cp.next() == it.next() == 2
+    assert cp.next() == it.next() == 1
+    assert cp.next() == it.next() == 2
+
+
+def test_cache_cycle_can_be_copied() -> None:
+    it = LIter.from_items(1, 2).cycle(use_cache=True)
+    it.next()
+
+    assert it.can_be_copied()
+
+    cp = it.copy()
+
+    assert cp.next() == it.next() == 2
+    assert cp.next() == it.next() == 1
+    assert cp.next() == it.next() == 2
