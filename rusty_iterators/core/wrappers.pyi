05b7fe44c6b3e11718150a82ecaa15b526c6a441
Wiktor Nowak <48101734+uncommon-nickname@users.noreply.github.com>
2025-02-09 11:36:08 +0100

    chore: remove redundant stub information (#147)
    * chore: remove redundant stub information
    
    * fix: after rebase

--- rusty_iterators/core/wrappers.pyi
+++ rusty_iterators/core/wrappers.pyi
@@ -3,7 +3,7 @@ from __future__ import annotations
 from collections.abc import Iterator, Sequence
 from typing import Any, Generic, final
 
-from rusty_iterators._versioned_types import TypeVar, override
+from rusty_iterators._versioned_types import TypeVar
 from rusty_iterators.core.interface import IterInterface
 
 T = TypeVar("T", default=Any, covariant=True)
@@ -11,15 +11,7 @@ T = TypeVar("T", default=Any, covariant=True)
 @final
 class SeqWrapper(IterInterface[T], Generic[T]):
     def __init__(self, s: Sequence[T]) -> None: ...
-    @override
-    def can_be_copied(self) -> bool: ...
-    @override
-    def copy(self) -> SeqWrapper[T]: ...
 
 @final
 class IterWrapper(IterInterface[T], Generic[T]):
     def __init__(self, it: Iterator[T]) -> None: ...
-    @override
-    def can_be_copied(self) -> bool: ...
-    @override
-    def copy(self) -> IterWrapper[T]: ...
