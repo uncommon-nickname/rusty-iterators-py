14a82ee85a7f89800dae2e680f7f3d9f4077e9f3
leghart <leghart@wp.pl>
2025-02-16 13:13:31 +0100

    fix

--- tests/unit/sync/test_inspect.py
+++ tests/unit/sync/test_inspect.py
@@ -6,9 +6,8 @@ from rusty_iterators import LIter
 
 def test_inspect_self() -> None:
     with io.StringIO() as buf, redirect_stdout(buf):
-        data = LIter.from_items(1, 2).inspect().map(lambda x: x * 2).collect()
+        LIter.from_items(1, 2).inspect().map(lambda x: x * 2).collect()
         assert buf.getvalue() == "Inspect(it=SeqWrapper(ptr=1, s=2)): 1\nInspect(it=SeqWrapper(ptr=2, s=2)): 2\n"
-    assert data == [2, 4]
 
 
 def test_inspect_function() -> None:
